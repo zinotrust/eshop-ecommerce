5810d24192054744139dff129b2650918adf406d
ZinoTrust <zinotrust@gmail.com>
2022-04-15 13:23:49 +0100

    6. Firebase Config setup

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAT6q9WGpjQrQDUtDHvljfonynkBKvMzI0",
+  authDomain: "eshop-ea8e7.firebaseapp.com",
+  projectId: "eshop-ea8e7",
+  storageBucket: "eshop-ea8e7.appspot.com",
+  messagingSenderId: "146569443233",
+  appId: "1:146569443233:web:46db000fc2c897f3b73de0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
