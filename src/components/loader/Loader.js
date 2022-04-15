80242f962063a95dbbbe402aef9dffb335e3f610
ZinoTrust <zinotrust@gmail.com>
2022-04-15 12:41:44 +0100

    5. Created loader component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,16 @@
+import styles from "./Loader.module.scss";
+import loaderImg from "../../assets/loader.gif";
+import ReactDOM from "react-dom";
+
+const Loader = () => {
+  return ReactDOM.createPortal(
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <img src={loaderImg} alt="Loading..." />
+      </div>
+    </div>,
+    document.getElementById("loader")
+  );
+};
+
+export default Loader;
