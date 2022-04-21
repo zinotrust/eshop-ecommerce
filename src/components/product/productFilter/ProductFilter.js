e6933a185a7aea2560554199bc2dcfb3666f84c7
ZinoTrust <zinotrust@gmail.com>
2022-04-21 13:46:15 +0100

    17. Product Filter JSX

--- src/components/product/productFilter/ProductFilter.js
+++ src/components/product/productFilter/ProductFilter.js
@@ -1,9 +1,26 @@
 import React from "react";
+import styles from "./ProductFilter.module.scss";
 
 const ProductFilter = () => {
   return (
-    <div>
-      <h2>Product Filter</h2>
+    <div className={styles.filter}>
+      <h4>Categories</h4>
+      <div className={styles.category}>
+        <button>All</button>
+      </div>
+      <h4>Brand</h4>
+      <div className={styles.brand}>
+        <select name="brand">
+          <option value="all">All</option>
+        </select>
+        <h4>Price</h4>
+        <p>1500</p>
+        <div className={styles.price}>
+          <input type="range" name="proce" min="100" max="1000" />
+        </div>
+        <br />
+        <button className="--btn --btn-danger">Clear Filter</button>
+      </div>
     </div>
   );
 };
