e213411fc3a0dfdb14484681521b2a8f942e105e
ZinoTrust <zinotrust@gmail.com>
2022-04-30 17:16:11 +0200

    make it better

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 const app = express();
 app.use(cors());
 app.use(express.json());
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname.at, "build", "index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("Welcome to eShop website.");
--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -38,8 +38,9 @@ const Checkout = () => {
   const description = `eShop payment: email: ${customerEmail}, Amount: ${totalAmount}`;
 
   useEffect(() => {
+    // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:4242/create-payment-intent", {
+    fetch("https://eshop-react-firebase.herokuapp.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
